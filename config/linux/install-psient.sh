0241d583849047ceff330587ca2d7ae3010c50b4
fvarrui <4354486+fvarrui@users.noreply.github.com>
2021-04-27 19:07:39 +0100

    U pseint binaries installation

---
+++ config/linux/install-psient.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Installing PSeInt ..."
+
+APP_NAME=pseint
+DOWNLOAD_URL=http://prdownloads.sourceforge.net/pseint/pseint-l64-20210408.tgz?download
+FILE=/tmp/$APP_NAME.tgz
+INSTALL_DIR=/opt
+
+wget -qO $FILE $DOWNLOAD_URL
+tar xvfz $FILE
+mv $APP_NAME $INSTALL_DIR
+
+cat > /usr/share/applications/$APP_NAME.desktop <<EOL
+[Desktop Entry]
+Name=PSeInt
+Type=Application
+Exec=$INSTALL_DIR/$APP_NAME/bin/wxPSeInt
+Terminal=false
+Icon=$INSTALL_DIR/$APP_NAME/imgs/logo.png
+Comment=PSeInt
+NoDisplay=false
+Categories=Development;IDE;
+EOL
