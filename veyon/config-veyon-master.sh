8eea2fcc2e8e63b6d17d617d3a4af7103204691f
fvarrui <4354486+fvarrui@users.noreply.github.com>
2021-05-07 13:35:12 +0100

    Update config-veyon-master.sh

--- veyon/config-veyon-master.sh
+++ veyon/config-veyon-master.sh
@@ -9,7 +9,7 @@ apt install -y veyon-master
 # Download and install master private key
 VEYON_KEYS_DIR=/etc/veyon/keys/private/informatica
 mkdir -p $VEYON_KEYS_DIR
-wget -qO $VEYON_KEYS_DIR/key https://raw.githubusercontent.com/iesdpm/informatica/master/keys/veyon/informatica_private_key.pem
+wget -qO $VEYON_KEYS_DIR/key https://raw.githubusercontent.com/iesdpm/informatica/master/veyon/keys/informatica_private_key.pem
 
 # Enable and start Veyon client service
 systemctl enable veyon
