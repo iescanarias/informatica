23c1eb2e73f6bf97439278835a16df753ee9968a
fvarrui <4354486+fvarrui@users.noreply.github.com>
2021-05-10 14:12:07 +0100

    U verbose script download

--- veyon/config-veyon-client.sh
+++ veyon/config-veyon-client.sh
@@ -9,7 +9,7 @@ apt install -y veyon-service
 
 # Download and install master public key
 VEYON_PUBLIC_KEY=/tmp/public_key.pem
-wget -qO $VEYON_PUBLIC_KEY https://raw.githubusercontent.com/iesdpm/informatica/master/veyon/keys/informatica_public_key.pem
+wget -O $VEYON_PUBLIC_KEY https://raw.githubusercontent.com/iesdpm/informatica/master/veyon/keys/informatica_public_key.pem
 veyon-cli authkeys import informatica/public $VEYON_PUBLIC_KEY
 veyon-cli authkeys setaccessgroup informatica/public sudo
 veyon-cli config set "Authentication/Method" 1
--- veyon/config-veyon-master.sh
+++ veyon/config-veyon-master.sh
@@ -9,7 +9,7 @@ apt install -y veyon-master
 
 # Download and install master private key
 VEYON_PRIVATE_KEY=/tmp/private_key.pem
-wget -qO $VEYON_PRIVATE_KEY https://raw.githubusercontent.com/iesdpm/informatica/master/veyon/keys/informatica_private_key.pem
+wget -O $VEYON_PRIVATE_KEY https://raw.githubusercontent.com/iesdpm/informatica/master/veyon/keys/informatica_private_key.pem
 veyon-cli authkeys import informatica/private $VEYON_PRIVATE_KEY
 veyon-cli authkeys setaccessgroup informatica/private sudo
 veyon-cli config set "Authentication/Method" 1
