d4292fb46e325f1a7e4e43e4007b03894bf514c6
Francisco Vargas Ruiz <4354486+fvarrui@users.noreply.github.com>
2024-10-17 22:32:31 +0100

    scripts de instalación para pcs de 1smr

---
+++ config/windows/1smr/README.md
@@ -0,0 +1,42 @@
+﻿# Configuración de equipos con Windows
+
+Para configurar los equipos con Windows instalado ejecutamos un script que hace lo siguiente:
+
+1. Instala el software de la siguiente [lista](packages.txt).
+
+2. Configura el sistema para que almacene los perfiles de los usuarios en la segundad unidad (unidad D:)
+   
+   > :warning: Sólo en caso de que se detecte una segunda unidad en el equipo.
+
+3. Crear el perfil de usuario `Alumno` (con privilegios administrativos).
+
+4. Programa el apagado del sistema a las 15:00 todos los días (al terminar las clases).
+
+5. Desinstala OneDrive.
+
+6. Cambia el nombre del equipo a `INFORMATICA` y mete el equipo en el grupo de trabajo `MINIK`.
+
+7. Cambia la zona horaria a GMT-0 (Atlantic/Canary).
+
+8. Deshabilita Windows Update.
+
+## Requisitos
+
+* Windows 7+/2012+
+* PowerShell v2+
+
+## Ejecución del script
+
+Ejecutar el siguiente comando como `Administrador` desde:
+
+### Símbolo del sistema (CMD):
+
+```
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/iesdpm/informatica/master/config/windows/config-computer.ps1'))"
+```
+
+### PowerShell (PS):
+
+```powershell
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/iesdpm/informatica/master/config/windows/config-computer.ps1'))
+```
---
+++ config/windows/1smr/config-computer.ps1
@@ -0,0 +1,103 @@
+﻿Write-Host @"
+
+Script de instalación de apps automática en Windows para los equipos de 1ºSMR.
+Departamento de Informática del IES Canarias
+
+"@
+
+# ----------------------------------
+# Users/groups related functions
+# ----------------------------------
+
+# Check if current user has admin privileges
+Function Test-RunningAsAdministrator() {
+    return (([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -Match "S-1-5-32-544")
+}
+
+# ----------------------------------
+# Chocolatey related functions
+# ----------------------------------
+
+# Checking if Chocolatey is installed
+function Test-ChocoInstalled() {
+    Write-Host -NoNewline "Checking if Chocolatey is installed ... "
+    $ChocoInstalled = $false
+    if (Get-Command choco.exe -ErrorAction SilentlyContinue) {
+        $ChocoInstalled = $true
+        Write-Host "[INSTALLED]"
+    } else {
+        Write-Host "[NOT INSTALLED]"
+    }
+    return $ChocoInstalled
+}
+
+# Install Chocolatey
+function Install-Choco() {
+    Write-Host "Installing Chocolatey..."
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+# Install a package with Chocolatey
+function Install-Package([string]$package) {
+    Write-Host -NoNewline "Installing $package ... "
+    choco install --ignore-checksum -y $package | Out-Null
+    If ($LASTEXITCODE -eq 0) {
+        Write-Host "[Ok]"
+    } else {
+        Write-Host "[Error]"
+    }
+}
+
+# ----------------------------------
+# Software installation related functions
+# ----------------------------------
+
+Function Install-Packages($packages) {
+
+    Write-Host "Installing packages ..."
+
+    # Instalación del gestor de paquetes Chocolatey
+    If (-Not (Test-ChocoInstalled)) {
+        Install-Choco
+    }
+
+    # Instalación de paquetes
+    $packages | ForEach-Object { 
+        Install-Package $_
+    }
+
+}
+
+Function Install-FromUrl($url) {
+
+    # get filename from url
+    $filename = [System.IO.Path]::GetFileName($url)
+    Write-Output "Installing $filename from $url"
+
+    # download file
+    Write-Output "Downloading $url"
+    $file = "$env:TEMP\$filename"
+    Invoke-WebRequest -Uri $url -OutFile $file
+
+    # run installer
+    Write-Output "Running installer $file"
+    Start-Process -FilePath $file -ArgumentList "/S" -Wait
+
+}
+
+# ----------------------------------
+# Main (installs all packages from the list)
+# ----------------------------------
+
+# Check if is running as administrator
+If (Test-RunningAsAdministrator) {
+    
+    # Packages installation
+    Install-Packages @( "virtualbox", "libreoffice-fresh" )
+    Install-FromUrl "https://github.com/iescanarias/informatica/releases/download/2024.10.17/pt822.exe"
+
+} Else {
+
+    Write-Host -ForegroundColor Red "Must be run as" (Get-AdminUsername)
+
+ }
